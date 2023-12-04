d4493b8507403469b8ea8e8f63af6c2b89f9c659
alessiafabbioni <alessiafabbioni@gmail.com>
2023-12-04 17:24:07 +0100

    show carousel

--- script.js
+++ script.js
@@ -6,11 +6,18 @@ Milestone 0:
 Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup statico: costruiamo il container e inseriamo l’immagine grande in modo da poter stilare lo slider.
 
 Milestone 1:
-Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.
-Al click dell’utente sulle frecce verso sinistra o destra, l’immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
+Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti 
+letterali per popolare dinamicamente il carosello.
+Al click dell’utente sulle frecce verso sinistra o destra, 
+l’immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
 
 Milestone 2:
-Aggiungere il **ciclo infinito** del carosello. Ovvero se la miniatura attiva è la prima e l’utente clicca la freccia verso destra, la miniatura che deve attivarsi sarà l’ultima e viceversa per l’ultima miniatura se l’utente clicca la freccia verso sinistra.
+Aggiungere il **ciclo infinito** del carosello. 
+Ovvero se la miniatura attiva è la prima e 
+l’utente clicca la freccia verso destra, 
+la miniatura che deve attivarsi sarà l’ultima 
+e viceversa per l’ultima miniatura se l’utente 
+clicca la freccia verso sinistra.
 
 BONUS 1:
 Aggiungere le thumbnails (sottoforma di miniatura) ed al click attivare l’immagine corrispondente.
@@ -19,25 +26,46 @@ Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi)
 BONUS 3: Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.*/
 
 
-/* const items = document.getElementsByClassName("item");
-console.log(items[0]);
 
-let activeItem = 0;
-
-const next = document.querySelector(".next");
-next.addEventListener('click', function () {
-
-    if (activeItem < items.length -1) {
-        items[activeItem].classList.remove('active');
-        activeItem++;
-        items[activeItem].classList.add('active')
-
-        if (activeItem === items.length -1) {
-            next.classList.add('hidden')
-        }
+const images = [
+    {
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
+];
 
+const slider = document.querySelector(".slider");
+const next = document.querySelector("#next");
+const prev = document.querySelector("#prev");
+
+let currentSlide = 0;
+
+function showPictures(index) {
+    const { image, title, text } = images[index];
+    const slidePicture = `<div>
+                            <img src="${image}" alt="">
+                            <div class="caption">
+                                <h2>${title}</h2>
+                                <p>${text}</p>
+                            </div>
+                        </div>`;
+                        
+    slider.innerHTML = slidePicture;
 }
-) */
-
-
